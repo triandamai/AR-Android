eae191af59b189657143052ee217c13d0aab801b
trian damai <triannurizkillah@gmail.com>
2023-06-07 22:31:11 +0700

    feat working on quiz

--- androidApp/src/main/java/app/hilwa/ar/MainActivity.kt
+++ androidApp/src/main/java/app/hilwa/ar/MainActivity.kt
@@ -1,6 +1,8 @@
 package app.hilwa.ar
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.LaunchedEffect
@@ -9,6 +11,7 @@ import app.hilwa.ar.base.EventListener
 import app.hilwa.ar.base.extensions.addOnEventListener
 import app.hilwa.ar.base.extensions.listenChanges
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -35,10 +38,43 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private val durationInSecond = (60 * 60L)
+    private val durationInMillis = TimeUnit.SECONDS.toMillis(durationInSecond)
+    private val countDown = object : CountDownTimer(durationInMillis, 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+
+            val formatString = String.format(
+                "%02d : %02d : %02d ",
+                TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                        TimeUnit.MINUTES.toSeconds(
+                            TimeUnit.MILLISECONDS.toMinutes(
+                                millisUntilFinished
+                            )
+                        )
+            )
+            eventListener.updateTimer(
+                false,
+                formatString
+            )
+        }
+
+        override fun onFinish() {
+            eventListener.updateTimer(
+                true
+            )
+        }
+
+    }
+
     private fun listen() {
         appState.addOnEventListener {
-            if (it == "EXIT") {
-                finish()
+            when (it) {
+                ApplicationStateConstants.CANCEL_TIMER -> countDown.cancel()
+                ApplicationStateConstants.EXIT_APP -> finish()
+                ApplicationStateConstants.FINISH_TIMER -> countDown.onFinish()
+                ApplicationStateConstants.START_TIMER -> countDown.start()
             }
         }
     }
