62d7644c8395ac11a307a5c9a701d8f3af5e30b5
trian damai <triannurizkillah@gmail.com>
2023-06-14 23:18:46 +0700

    feat working on quiz

--- androidApp/src/main/java/app/hilwa/ar/components/HeaderStepWithProgress.kt
+++ androidApp/src/main/java/app/hilwa/ar/components/HeaderStepWithProgress.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import app.hilwa.ar.base.listener.AREventListener
 import app.trian.core.ui.BaseMainApp
 
 @Composable
