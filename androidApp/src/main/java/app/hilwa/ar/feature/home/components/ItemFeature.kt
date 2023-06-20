691a456d58c7c32c937014aba7e92bc2de65b36f
trian damai <triannurizkillah@gmail.com>
2023-06-20 22:12:20 +0700

    refactor: use jitpack for mvi

--- androidApp/src/main/java/app/hilwa/ar/feature/home/components/ItemFeature.kt
+++ androidApp/src/main/java/app/hilwa/ar/feature/home/components/ItemFeature.kt
@@ -26,14 +26,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import app.hilwa.ar.R
