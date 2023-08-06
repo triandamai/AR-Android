9806c6a1ddd2ee27b6e3d5d0ae82c8aadd7f1a34
IBM Trian Damai <triannurizkillah@gmail.com>
2023-08-06 22:26:15 +0700

    update profile(domain)

--- androidApp/src/main/java/app/hilwa/ar/feature/auth/changePassword/ChangePasswordScreen.kt
+++ androidApp/src/main/java/app/hilwa/ar/feature/auth/changePassword/ChangePasswordScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
@@ -15,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
